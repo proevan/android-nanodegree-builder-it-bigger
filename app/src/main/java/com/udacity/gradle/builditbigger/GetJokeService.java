006c55a1be27f14882a2746729fe408c20f6997a
proevan <yanghsin.lin@gmail.com>
2015-07-19 18:39:00 +0800

    use Async task inside GetJokeService

--- app/src/main/java/com/udacity/gradle/builditbigger/GetJokeService.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/GetJokeService.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.os.AsyncTask;
 import android.util.Log;
 
 import com.proevan.nanodegree.builditbigger.backend.jokeApi.JokeApi;
@@ -15,18 +16,33 @@ public class GetJokeService {
     }
 
     public void getJoke(final Callback callback) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                String result = null;
-                try {
-                    result =  mJokeApiService.getJoke().execute().getText();
-                } catch (IOException e) {
-                    Log.e("GetJokeService", "getJoke IOException occured");
-                }
-                callback.onComplete(result);
+        new GetJokeAsyncTask(mJokeApiService, callback).execute();
+    }
+
+    static class GetJokeAsyncTask extends AsyncTask<Void, Void, String> {
+
+        private JokeApi mJokeApiService;
+        private Callback mCallback;
+
+        public GetJokeAsyncTask(JokeApi jokeApiService, Callback callback) {
+            mJokeApiService = jokeApiService;
+            mCallback = callback;
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                return mJokeApiService.getJoke().execute().getText();
+            } catch (IOException e) {
+                Log.e("GetJokeService", "getJoke IOException occured");
+                return null;
             }
-        }).start();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            mCallback.onComplete(result);
+        }
     }
 
     public interface Callback {
